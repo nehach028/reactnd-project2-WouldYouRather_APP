9468553691787ac1dca525abb2cd8c3f96213e5f
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 15:26:04 +1000

    Update : updated few rout path & added Proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,52 @@
 import React, { Component,Fragment } from 'react';
-import { BrowserRouter as Router,Route,Redirect,Switch } from 'react-router-dom'
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
-import '../App.css';
+
 import { handleInitialData } from "../actions/shared";
 import Nav from  './Nav'
 import SignIn from './SignIn';
 import Home from './Home';
-import Logout from './Logout';
 import Question from './Question';
 import ViewPollResult from './ViewPollResult';
+import NewQuestion from './NewQuestion';
+import Error404 from './Error404'
+import LeaderBoard from './LeaderBoard';
+import LoadingBar from 'react-redux-loading'
+
+import 'semantic-ui-css/semantic.min.css';
+import '../App.css';
 
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleInitialData())
   }
   render() {
-   
-    return (
+   return (
       <Router>
         <Fragment>
-          <Nav/>
-          
-                
-                <Switch>
+        <LoadingBar style={{ backgroundColor: 'teal', height: '3px' , position: 'absolute' }} /> 
+        <Nav/>
+          {this.props.loading===true
+            ? <Route path ='/' component={SignIn}/>
+            :    <Switch>
                   <Route path ='/' exact component={SignIn}/>
                   <Route path ='/home' exact component={Home}/>
-                  <Route path ='/signin' exact component={SignIn}/>
-                  <Route path ='/logout' exact component={Logout}/>
-                  <Route path ='/question/:question_id' exact component={Question}/>
-                  <Route path ='/viewpoll' exact component={ViewPollResult}/>
-                  <Redirect to= '/'/>
+                  <Route path ='/signin' exact  component={SignIn}/>
+                  <Route path ='/question/:question_id'  exact component={Question}/>
+                  <Route path ='/viewpoll/:question_id'  exact component={ViewPollResult}/>
+                  <Route path ='/add'  exact component={NewQuestion}/>
+                  <Route path ='/leaderboard' exact component={LeaderBoard} />
+                  <Route path ='/error' exact component={Error404}/>
                 </Switch>
-              
-            
+          }
         </Fragment>
       </Router>
     );
   }
+}
+App.propTypes = {
+	dispatch: PropTypes.func.isRequired,
 }
 function mapStateToProps({authedUser}){
   return{
