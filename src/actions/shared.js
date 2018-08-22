2bf190880e5b7912d24b15c4689a149963e9be97
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 15:22:35 +1000

    Add : Added Loading Bar dispatch call

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,15 +1,16 @@
 import { getInitialData } from "../utils/api";
 import { receiveQuestions } from "../actions/questions";
 import { receiveUsers } from "../actions/users";
+import { hideLoading, showLoading } from 'react-redux-loading'
 //Thunk action creator:Asynchronous request to get initial data
 export function handleInitialData(){
-   
     return(dispatch)=>{
+        dispatch(showLoading())
         return getInitialData()
         .then(({users,questions})=>{
             dispatch(receiveUsers(users))
             dispatch(receiveQuestions(questions))
-            //dispatch(setAuthedUser(id))
+            dispatch(hideLoading())
         })
     }
 }
