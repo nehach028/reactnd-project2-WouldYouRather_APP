8543af89e1fe146325b599b1d26b2b3d84edd310
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 16:14:56 +1000

    Update : Formate percentage value

--- src/components/ViewPollResult.js
+++ src/components/ViewPollResult.js
@@ -1,51 +1,65 @@
-import React, { Component } from 'react'
-import { Segment,Item, Header ,Grid,Container,Progress} from "semantic-ui-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Segment, Item, Header, Grid, Container, Progress, Icon, Label} from "semantic-ui-react";
 import { connect } from 'react-redux';
-class ViewPollResult extends Component{
-    render(){
-        const { question, user} =this.props
-        const { name,avatarURL}=user
+import { Redirect } from "react-router-dom";
+
+const ViewPollResult = (props) => {
+        const { question, user, yourvote} = props
+        //To display 404 page ,If poll not found
+        if(question==='undefined'|| user===null){
+            return <Redirect exact to='/error'/>
+        }
+        const { name,avatarURL} = user
         let TotalVote = 0
         let percentOne=0
         let percentTwo=0
         let voteCountOne=question.optionOne.votes.length
         let  voteCountTwo=question.optionTwo.votes.length
+        // calculating the percentage
         TotalVote=voteCountOne + voteCountTwo
-        percentOne=(voteCountOne*100) / (TotalVote)
-        percentTwo=(voteCountTwo*100) / (TotalVote)
-        console.log("percentOne" ,percentOne )
-        console.log("percentTwo",percentTwo)
+        percentOne=Math.round((voteCountOne*100) / (TotalVote) * 100) / 100
+        percentTwo=Math.round((voteCountTwo*100) / (TotalVote) * 100) / 100
         return(
             <div>
-            <Container style={{ marginTop: '3em' }}>
+            <Container style={{ marginTop: '3em',width:600 }}>
             <Grid  verticalAlign='middle' stackable columns='equal' divided>
                 <Grid.Row >
                     <Grid.Column >
                     <Header as='h4' attached='top' block>
-                    Asked by { name } 
+                        Asked by { name }
                     </Header>
                     <Segment attached>
-                    <Item.Group relaxed>
-                        <Item>
-                        <Item.Image size='small' src={avatarURL} />
-                        
-                        <Item.Content verticalAlign='middle'>
-                            <Item.Header> Results </Item.Header>
-                            <Item.Description>
-                            <Segment  style={{color:'teal'}}>
-                                Would you rather {question.optionOne.text}?
-                                <Progress percent={`${percentOne}`} progress color='teal' style={{ marginTop: '3em' }} />
-                                <span>{voteCountOne} out of {TotalVote} votes</span>
-                            </Segment>
-                            <Segment>
-                                Would you rathet {question.optionTwo.text}?
-                                <Progress percent={`${percentTwo}`} progress color='teal' style={{ marginTop: '3em' }} />
-                                <span>{voteCountTwo} out of {TotalVote} votes</span>
-                            </Segment>        
-                            </Item.Description>
-                        </Item.Content>
-                        </Item>
-                    </Item.Group>    
+                        <Item.Group relaxed>
+                            <Item>
+                            <Item.Image size='small' src={avatarURL}  />
+                            <Item.Content verticalAlign='middle'>
+                                <Item.Header> Results: </Item.Header>
+                                <Item.Description>
+                                <Segment >
+                                    <Header as='h5'>
+                                        { yourvote==='optionOne'? ( <Label as='a' color='orange' ribbon><Icon name='star'/>Your Vote</Label>)
+                                                                    : ''
+                                        }
+                                        <Header.Content>Would you rather {question.optionOne.text}?</Header.Content>
+                                    </Header>
+                                    <Progress percent={`${percentOne}`} progress color='teal' style={{ marginTop: '1em' }} />
+                                    <span>{voteCountOne} out of {TotalVote} votes</span>
+                                </Segment>
+                                <Segment>
+                                    <Header as='h5'>
+                                        { yourvote==='optionTwo'? ( <Label as='a' color='orange' ribbon><Icon name='star'/>Your Vote</Label>)
+                                                                    : ''
+                                        }
+                                        <Header.Content >Would you rather {question.optionTwo.text}?</Header.Content>
+                                    </Header>
+                                    <Progress percent={`${percentTwo}`} progress color='teal' style={{ marginTop: '1em' }} />
+                                    <span>{voteCountTwo} out of {TotalVote} votes</span>
+                                </Segment>
+                                </Item.Description>
+                            </Item.Content>
+                            </Item>
+                        </Item.Group>
                         </Segment>
                     </Grid.Column>
                 </Grid.Row>
@@ -53,15 +67,30 @@ class ViewPollResult extends Component{
         </Container>
         </div>
         )
-    }
 }
-function mapStateToProps({questions,users},props){
+//Required PropTypes:
+ViewPollResult.propTypes = {
+    question_id : PropTypes.string,
+    user : PropTypes.object,
+    question: PropTypes.object,
+    yourvote : PropTypes.string,
+}
+ViewPollResult.defaultProps  = {
+    user : null,
+    yourvote : '',
+}
+
+function mapStateToProps({questions,users,authedUser},props){
     const {question_id}=props.match.params
     const question=questions[question_id]
-    const user=users[question.author]
+    const user=question?users[question.author]:null
+    //To get the option,selected by loggedin user
+    const yourvote=users[authedUser].answers.hasOwnProperty(question_id) ?
+                                    users[authedUser].answers[question_id] : ''
     return {
         question,
         user,
+        yourvote,
     }
 }
 export default connect(mapStateToProps)(ViewPollResult)
