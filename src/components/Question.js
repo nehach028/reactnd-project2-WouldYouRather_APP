53d8648aac8456093e097b82921ff18bd85f8093
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 15:55:08 +1000

    Add : functionality  to display 404 page

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { withRouter,Redirect } from "react-router-dom";
+import PropTypes from 'prop-types';
 import { Segment,Item, Header ,Button,Grid,Form,Radio,Container} from "semantic-ui-react";
 import { handleAddQuestionAnswer} from '../actions/questions'
+
 class Question extends Component{
     state = {
         answer:'',
         toViewpoll:false
+    }
+    //Required prop types:
+    static propTypes={
+        dispatch : PropTypes.func.isRequired,
+        user : PropTypes.object,
+        question : PropTypes.object,
+    }
+    static defaultProps  = {
+        user: null,
+        question : null,
     }
     handleChange = (e, { value }) => this.setState({answer:value })
     handleSubmit=(e)=>{
@@ -14,27 +26,29 @@ class Question extends Component{
         const { answer } =this.state
         const { dispatch,question} = this.props
         const qid=question.id
-        
-        console.log(qid,"questionid")
-        console.log(answer,"option")
         //dispatching an action
         dispatch(handleAddQuestionAnswer({qid,answer}))
         this.setState({
             toViewpoll:true
         })
-        //this.props.history.push('/viewpoll');
     }
     render(){
         const {question,user} = this.props
+        //If poll does not exist
+        if(question==='undefined'|| user===null){
+            return <Redirect exact to='/error'/>
+        }
+
         const { name,avatarURL}=user
+        const {question_id}=this.props.match.params
         const optionOne=question.optionOne.text
         const optionTwo=question.optionTwo.text
-         if(this.state.toViewpoll===true){
-            return <Redirect to='/viewpoll' />
-        } 
+        if(this.state.toViewpoll===true){
+            return <Redirect exact to={`/viewpoll/${question_id}`} />
+        }
         return(
             <div>
-                <Container style={{ marginTop: '3em' }}>
+                <Container style={{ marginTop: '3em',width:600 }}>
                 <Grid  verticalAlign='middle' stackable columns='equal'>
                     <Grid.Row >
                         <Grid.Column >
@@ -67,7 +81,7 @@ class Question extends Component{
                                             onChange={this.handleChange}
                                         />
                                         </Form.Field>
-                                        <Button fluid color='teal'onClick={(e)=>this.handleSubmit(e)}>Submit</Button>
+                                        <Button fluid color='teal' disabled={this.state.answer === ''}onClick={(e)=>this.handleSubmit(e)}>Submit</Button>
                                     </Form>
                                 </Item.Description>
                             </Item.Content>
@@ -85,7 +99,7 @@ class Question extends Component{
 function mapStateToProps({questions,users},props){
     const {question_id}=props.match.params
     const question=questions[question_id]
-    const user=users[question.author]
+    const user=question?users[question.author]:null
     return{
         question,
         user,
