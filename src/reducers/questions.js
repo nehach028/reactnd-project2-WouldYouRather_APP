f6c69c4e79d96c914366fae47b7e1215f3263da4
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 16:17:52 +1000

    Add : New Case for create new question

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -3,6 +3,7 @@
 */
 import { RECEIVE_QUESTIONS } from "../actions/questions";
 import { ADD_QUESTION_ANSWER } from "../actions/questions";
+import { ADD_QUESTION } from "../actions/questions";
 export default function questions(state={},action){
     switch(action.type){
         case RECEIVE_QUESTIONS :
@@ -10,7 +11,7 @@ export default function questions(state={},action){
              ...state,
              ...action.questions   
             }
-            case ADD_QUESTION_ANSWER :
+        case ADD_QUESTION_ANSWER :
             const { authedUser,qid,answer } = action
             return{
                 ...state,
@@ -21,6 +22,12 @@ export default function questions(state={},action){
                         votes: state[qid][answer].votes.concat([authedUser]),
                     },
                 },
+            }
+        case ADD_QUESTION :
+            const { question } = action
+        return{
+                ...state,
+                [question.id]: question,
             }
         default :
             return state
