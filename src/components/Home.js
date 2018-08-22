fa8e64924d392413680997bac4f6465550e70165
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 15:31:23 +1000

    Refactor: Refctore code, removed console logs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React , { Component } from 'react'
 import { connect } from "react-redux";
+import PropTypes from 'prop-types';
 import { Button, Container, Grid, Segment } from 'semantic-ui-react'
 import QuestionItem from './QuestionItem'
+
 class Home extends Component{
+    
     state={showUnanswered:true}
+    
     handleUnanswered=(e)=>{
         e.preventDefault()
         this.setState({showUnanswered:true})
@@ -13,24 +17,20 @@ class Home extends Component{
         this.setState({showUnanswered:false})
     }
     render(){
-        const { questionIds,users,answers } = this.props
+        const { questionIds, answers } = this.props
         const { showUnanswered } = this.state
         
-        console.log(users)
-        console.log(answers,"answers") 
         //Filter unanswered question of authed user
         const unanweredQ=questionIds.filter((ele)=>
                             {
-                                return answers.indexOf(ele)<0 
-                            },answers)
+                                return Object.keys(answers).indexOf(ele)<0 
+                            },Object.keys(answers))
+        const answeredQ=questionIds.filter((id)=>{
+                            return answers.hasOwnProperty(id)
+                        })    
             
-            
-        
-        console.log(unanweredQ,"unanweredQ")
-        
-        console.log("props",this.props)
         return(
-            <Container style={{ marginTop: '3em' }}>
+            <Container style={{ marginTop: '3em' ,width:600}}>
                 <Grid columns='equal' >
                     <Grid.Column>
                         <Button.Group attached='top' widths={2} inverted >
@@ -40,7 +40,7 @@ class Home extends Component{
                         <Segment attached>
                             {showUnanswered===true
                                 ? unanweredQ.map((qid)=>(<QuestionItem key={qid} question_id={qid} unansweredPoll={true}/>))
-                                :  answers.map((qid)=>(<QuestionItem key={qid} question_id={qid} unansweredPoll={false}/>))
+                                :  answeredQ.map((qid)=>(<QuestionItem key={qid} question_id={qid} unansweredPoll={false}/>))
                             }
                         </Segment>
                 </Grid.Column>
@@ -49,17 +49,17 @@ class Home extends Component{
         )
     }
 }
+Home.propTypes = {
+    questionIds : PropTypes.array.isRequired,
+    answers : PropTypes.object.isRequired,
+}
 function mapStateToProps({users,questions,authedUser}){
-    const author=(!authedUser)
+    const answers=(!authedUser)
                     ? []
                     :users[authedUser].answers
-    const answers=(!author)
-                  ? []
-                  :Object.keys(author).sort((a,b)=>(author[b].timstamp-author[a].timestamp))
     return{
-        questionIds:Object.keys(questions)
-                    .sort((a,b)=>(questions[b].timstamp-questions[a].timestamp)),
-        users:Object.keys(users).map((id)=>id),
+        questionIds : Object.keys(questions)
+                    .sort((a,b)=>questions[b].timestamp-questions[a].timestamp),
         answers,
     }
 }
