812a461bd23cdc7dda777df1bf41f8cc63038241
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-22 15:21:30 +1000

    Add : Added New action creators

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,11 @@
 /* @author: Neha Chaturvedi 7/27/2018
-@discription:Action creator to dispatch recive questions action
+@discription:Action creator to dispatch various question related actions
 */
-import { saveQuestionAnswer } from "../utils/api";
+import { saveQuestionAnswer , saveQuestion } from "../utils/api";
+import { hideLoading, showLoading } from 'react-redux-loading'
 export const RECEIVE_QUESTIONS='RECEIVE_QUESTIONS'
 export const ADD_QUESTION_ANSWER='ADD_QUESTION_ANSWER'
+export const ADD_QUESTION='ADD_QUESTION'
 
 export function receiveQuestions(questions){
     return{
@@ -24,7 +26,7 @@ export function handleAddQuestionAnswer({qid,answer}){
     return (dispatch,getState)=>{
         const {authedUser}=getState()
         
-        console.log({qid,answer})
+        dispatch(showLoading())
        
         return saveQuestionAnswer({
             authedUser,
@@ -35,9 +37,39 @@ export function handleAddQuestionAnswer({qid,answer}){
             qid,
             answer,
         })))
+        .then(() => dispatch(hideLoading()))
         .catch((e)=>{
             console.warn("Error in adding answer: ",e)
-            alert('There was an error adding the answer Try again')
+            alert('There was an error while adding the answer Try again')
+        })
+    }
+}
+
+//Action creator to dispatch new question
+
+export function addQuestion(question){
+    return {
+        type:ADD_QUESTION,
+        question,
+    }
+}
+
+//Middleware for achynchronous call
+
+export function handleAddQuestion(optionOneText,optionTwoText){
+    return(dispatch,getState)=>{
+        const { authedUser } = getState()
+        dispatch(showLoading())
+        return saveQuestion({
+            optionOneText,
+            optionTwoText,
+            author:authedUser,
+        })
+        .then((question)=>dispatch(addQuestion(question)))
+        .then(() => dispatch(hideLoading()))
+        .catch((e)=>{
+            console.warn("Error in saving Question: ",e)
+            alert('There was an error while adding the question Try again')
         })
     }
 }
