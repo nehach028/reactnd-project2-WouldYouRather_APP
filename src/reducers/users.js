6fee89daf67b11cbf4ab586e1133354543b9dadf
nehach028 <neha.chaturvedi028@gmail.com>
2018-08-23 22:53:50 +1000

    Fix : Updated user slice of state on adding new question.

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,7 +2,7 @@
 @discription:Users Reducer
 */
 import { RECEIVE_USERS } from "../actions/users";
-import { ADD_QUESTION_ANSWER } from "../actions/questions";
+import { ADD_QUESTION_ANSWER,ADD_QUESTION } from "../actions/questions";
 
 export default function users(state={},action){
     switch(action.type){
@@ -23,6 +23,15 @@ export default function users(state={},action){
                     },
                 },
             }
+        case ADD_QUESTION :
+            const { question } = action
+            return{
+                    ...state,
+                    [question.author]:{
+                        ...state[question.author],
+                        questions:state[question.author].questions.concat([question.id]),
+                        },
+                }
         default :
             return state
     }
